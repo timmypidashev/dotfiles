4399edb69cbf5a1ae33fa109922553a1208b984a
Timothy Pidashev <pidashev.tim@gmail.com>
2021-10-23 23:46:29 +0000

    Update install_on_arch.sh

--- install_on_arch.sh
+++ install_on_arch.sh
@@ -2,3 +2,4 @@ wallset deps: ffmpeg, feh, imagemagick, xorg-xrandr, xdg-utils, sed
 video driver: xf86-vdeo-amd
 aur helper: paru
 base-system: dont forget ot include base-devel
+xmonad: xmonad, xmonad-contrib
