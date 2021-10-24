3ea852c284d3c69913a837d90dfad9adcea2ed51
Timothy Pidashev <pidashev.tim@gmail.com>
2021-10-24 02:06:55 +0000

    Update install_arch.sh

--- scripts/install_arch.sh
+++ scripts/install_arch.sh
@@ -2,15 +2,6 @@
 
 echo "Timmy's Arch Installer"
 
-# Make sure we are connected to the internet
-read -p "Are you connected to internet? [y/N]: " connection
-if ! [ $connection = "y" ] && ! [ $connection = "Y" ]
-then 
-    echo "Connect to internet to continue..."
-    exit
-fi
-
-
 # Figure out if we are running on a Legacy or UEFI system
 echo "1) UEFI System 	2) Legacy System"
 read -r -p "What kind of computer are you running on? (default 1): " system
