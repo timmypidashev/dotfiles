f830a4efc6d00c2124e10fee728edec5b3a54463
Timothy Pidashev <pidashev.tim@gmail.com>
2021-10-24 02:09:53 +0000

    Update install_arch.sh

--- scripts/install_arch.sh
+++ scripts/install_arch.sh
@@ -4,9 +4,9 @@ echo "Timmy's Arch Installer"
 
 # Figure out if we are running on a Legacy or UEFI system
 echo "1) UEFI System 	2) Legacy System"
-read -r -p "What kind of computer are you running on? (default 1): " system
+read -r -p "What kind of computer are you running on? (default 1): " sys
 
-case $system in 
+case $sys in 
 [1])
     SYSTEM="UEFI"
     ;;
@@ -19,4 +19,4 @@ case $system in
 esac
 
 echo "Proceeding to install Arch Linux!"
-
+echo $SYSTEM
