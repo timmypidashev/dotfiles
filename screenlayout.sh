4af19a7e1db956f6167c4463a7a3fd2b27ed09ef
Timothy Pidashev <pidashev.tim@gmail.com>
2022-04-06 23:05:28 -0700

    Create screenlayout.sh

---
+++ screenlayout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --mode 1920x1080 --pos 5600x1703 --rotate normal --output HDMI-0 --primary --mode 3440x1440 --pos 2160x1477 --rotate normal --output DP-0 --mode 3840x2160 --pos 0x0 --rotate left --output DP-1 --off
